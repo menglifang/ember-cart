ba5f772dfd19cc8354ecbf364cea1bec29d52aa9
Tower He <towerhe@gmail.com>
2012-04-09 18:17:56 +0800

    Enabled controller extensions

--- lib/ember-cart.rb
+++ lib/ember-cart.rb
@@ -3,8 +3,7 @@ require 'acts_as_api'
 require 'ember_cart/engine'
 require 'ember_cart/cartable_validator'
 require 'ember_cart/acts_as_cartable'
+require 'ember_cart/controller_extensions'
 
 module EmberCart
 end
-
-ActiveRecord::Base.extend(EmberCart::ActsAsCartable)

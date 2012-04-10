b4815d2803154491d585929823e21e142c074c34
Tower He <towerhe@gmail.com>
2012-04-10 17:20:37 +0800

    Extract methods

--- lib/ember_cart/controller_extensions.rb
+++ lib/ember_cart/controller_extensions.rb
@@ -2,18 +2,42 @@ module EmberCart
   module ControllerExtensions
     extend ActiveSupport::Concern
 
+    def load_or_create_carts
+      @carts ||= ember_carts
+    end
+
     def ember_carts
       shopper = defined?(current_user) ? current_user : nil
 
-      @_carts = Cart.of(shopper) if @_carts.blank?
-      @_carts = default_carts_of(shopper) if @_carts.blank?
+      @carts = load_carts_for(shopper) if @carts.blank?
+      @carts = load_carts_from_cookies if @carts.blank?
+      @carts = create_carts_for(shopper) if @carts.blank?
 
-      @_carts
+      save_carts_to_cookies
+
+      @carts
+    end
+
+    def current_cart
+      Cart.find(cookies[:current_cart_id])
     end
 
     protected
-    def default_carts_of(shopper)
+    def create_carts_for(shopper)
       [Cart.create(name: 'Default', shopper: shopper)]
+    end
+
+    private
+    def load_carts_for(shopper)
+      shopper ? Cart.of(shopper) : []
+    end
+
+    def load_carts_from_cookies
+      Cart.find(cookies[:ember_carts_ids]) if cookies[:ember_carts_ids]
+    end
+
+    def save_carts_to_cookies
+      cookies[:ember_carts_ids] = @carts.map(&:id)
     end
   end
 end
